b2e37566ed6c79925586998419f419775bcde21b
최정헌 <iiaii@zuminternet.com>
2020-07-06 17:22:13 +0900

    todolist 구현중

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
-      list
+      <ul>
+          <li v-for="todoItem in todoItems" v-bind:key="todoItem">1</li>
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
+    data: function() {
+        return {
+            todoItems: []
+        }
+    },
 
+    created: function() {
+        if(localStorage.length > 0) {
+            for(let i=0 ; i<localStorage.length ; i++) {
+                if(localStorage.key(i) === 'loglevel:webpack-dev-server') {
+                    continue;
+                }
+                this.todoItems.push(localStorage.key(i))
+            }
+        }
+
+    }
 }
 </script>
 
